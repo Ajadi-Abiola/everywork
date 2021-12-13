4d20ec1845dd019b36da3ef091a52273bceaac94
Ajadi-Abiola <briannaajadi03@gmail.com>
2021-12-13 12:52:59 +0100

    first commit

---
+++ packages/builder-api/src/App.js
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  const add = require('page-console');
+
+  return (
+    <div className="App">
+      
+      <div>{add(1, 2)}</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/page-console/src/App.js
@@ -0,0 +1,3 @@
+module.exports = () => {
+  return ["🎨", "💰", "🎩", "🎒", "🧿"][Math.floor(Math.random() * 5)];
+};
